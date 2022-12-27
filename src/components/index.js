3c17b72d8f2bed95b9bec4c6e511ea589f4d4199
dario <nahuelperlapacheco2000@gmail.com>
2022-12-27 03:05:47 -0300

    entrega

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as ImageSelector } from "./image-selector";
+export { default as PlaceItem } from "./place-item";
+export { default as LocationSelector } from "./location-selector";
---
+++ src/screens/new-place/index.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { View, Text, ScrollView, TextInput, Button } from "react-native";
+import { useDispatch } from "react-redux";
+
+import { ImageSelector, LocationSelector } from "../../components";
+import { savePlace } from "../../store/place.slice";
+import colors from "../../utils/colors";
+import { styles } from "./styles";
+
+const NewPlace = ({ navigation }) => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState(null);
+  const dispatch = useDispatch();
+
+  const onHandleSubmit = () => {
+    dispatch(savePlace({ title, image }));
+    navigation.navigate("Places");
+  };
+
+  const onHandleChange = (text) => {
+    setTitle(text);
+  };
+
+  const onImagePicker = (uri) => {
+    setImage(uri);
+  };
+  const onLocationPicker = ({ lat, lng }) => {};
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Titulo</Text>
+        <TextInput
+          onChangeText={onHandleChange}
+          style={styles.input}
+          placeholder="Escribe el lugar"
+        />
+        <ImageSelector onImagePicker={onImagePicker} />
+        <LocationSelector onLocationPicker={onLocationPicker} />
+        <Button color={colors.primary} title="Guardar direccion" onPress={onHandleSubmit} />
+      </View>
+    </ScrollView>
+  );
+};
+
+export default NewPlace;
